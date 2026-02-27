099f3f6ffac6a47f76debe2d12f451df4cec943f
BB <8081834+s41nn0n@users.noreply.github.com>
2026-02-27 10:48:30 +0200

    chore: added simple test for ui

---
+++ plugins/ui/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/mywio/git-ops/pkg/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUIPlugin(t *testing.T) {
+	// Verify Plugin variable implements interface
+	var _ core.Plugin = Plugin
+
+	assert.Equal(t, "ui", Plugin.Name())
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	ctx := context.Background()
+
+	// Use ModuleManager as a dummy registry
+	mgr := core.NewModuleManager(logger)
+
+	err := Plugin.Init(ctx, logger, mgr)
+	assert.NoError(t, err)
+
+	err = Plugin.Start(ctx)
+	assert.NoError(t, err)
+
+	caps := Plugin.Capabilities()
+	assert.Contains(t, caps, core.CapabilityUI)
+	assert.Contains(t, caps, core.CapabilityAPI)
+
+	status := Plugin.Status()
+	assert.Equal(t, core.StatusHealthy, status)
+
+	err = Plugin.Stop(ctx)
+	assert.NoError(t, err)
+}
